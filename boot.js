d682814b165443a05ee2b94b728c7282b90a3bbf
felipematheus1337 <lipehbr@gmail.com>
2022-10-17 12:04:20 -0300

    Entidades criadas

--- boot.js
+++ boot.js
@@ -1,5 +1,17 @@
 module.exports = app => {
-    app.listen(app.get('port'), () => {
-        console.log(`NTask Running with port : ${app.get('port')}`);
-    })
+   async function start(port) {
+    try {
+        await app.db.authenticate();
+        await app.db.sync();
+        app.listen(port, () => {
+            console.log("Ntask API with PORT "+port);
+        });
+        
+    } catch(err) {
+            console.log("Erro de conexão com banco de dados!")
+            console.log(err);
+    }
+   }
+
+   start(app.get('port'))
 }
