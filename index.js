93a4ada6c6c5843c8c7313e2344ea9a6809e92f1
felipematheus1337 <lipehbr@gmail.com>
2022-10-17 11:11:13 -0300

    cosign adicionado, criado pasta Models

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require("express");
-
+const consign = require("consign");
 const PORT = 3000;
 const app = express();
 
-app.get("/",(req,res) => {
-    res.json({status: " NTask API"})
-});
 
 app.set('json spaces',4);
 
-app.get('/tasks',(req,res) => {
-    res.json({
-        tasks: [
-            {title:'Fazer compras'},
-            {title: 'Consertar o pc'}
-        ]
-    });
-});
+consign()
+ .include('models')
+ .include('routes')
+ .into(app);
+
+
 
 
 app.listen(PORT,() => {
